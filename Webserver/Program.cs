aa8dfd9dabc115e02b02bf9dfe4cfa1bf9e12eda
Nor Shiervani <50927017+NorShiervani@users.noreply.github.com>
2020-09-01 21:15:55 +0200

    Cookies feature with the assistance from code by IzC87.

--- Webserver/Program.cs
+++ Webserver/Program.cs
@@ -1,14 +1,18 @@
-﻿using Microsoft.AspNetCore.StaticFiles;
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.StaticFiles;
+using Microsoft.Net.Http.Headers;
 using System;
 using System.IO;
 using System.Net;
-using System.Web;
+using System.Security.Cryptography;
+using System.Text;
 
 namespace Webserver
 {
     class Program
     {
         private static int counter = 0;
+
         static void Main(string[] args)
         {
             SimpleListenerExample(new string[] { "http://localhost:8080/" });
@@ -35,39 +39,63 @@ namespace Webserver
             }
             listener.Start();
             Console.WriteLine("Listening...");
-            while (true) {
+            ProcessHttpListener(listener);
+        }
+
+        private static void ProcessHttpListener(HttpListener listener)
+        {
+            while (true)
+            {
                 // Note: The GetContext method blocks while waiting for a request.
                 HttpListenerContext context = listener.GetContext();
                 HttpListenerRequest request = context.Request;
                 // Obtain a response object.
                 HttpListenerResponse response = context.Response;
-
                 // Construct a response.
                 string filePath = Path.Combine(Directory.GetCurrentDirectory() + request.RawUrl);
-                byte[] buffer = File.ReadAllBytes(filePath);
+                byte[] buffer;
+                Stream stream = response.OutputStream;
+
                 counter++;
-                
+                response.Cookies.Add(new Cookie("counter", counter.ToString()));
+                if (request.RawUrl == "/counter")
+                {
+                    buffer = Encoding.ASCII.GetBytes(response.Cookies["counter"].Value);
+                }
+                else
+                {
+                    buffer = File.ReadAllBytes(filePath);
+                    response.AddHeader("Content-Type", GetContentType(filePath));
+                    response.AddHeader("ETag", "\"" + CalculateMD5Hash(filePath) + "\"");
+                }
+
                 response.ContentLength64 = buffer.Length;
                 response.AddHeader("Expires", DateTime.Now.AddYears(1).ToString());
                 response.AddHeader("StatusCode", response.StatusCode.ToString());
-                response.AddHeader("Content-Type", GetContentType(filePath));
-
-                string contentType = GetContentType(filePath);
-                Console.WriteLine("Counter: " + counter + " " + contentType);
-
-                Stream stream = response.OutputStream;
                 stream.Write(buffer, 0, buffer.Length);
                 stream.Close();
-                
-                //listener.Stop();
             }
 
+            //listener.Stop();
         }
+
         private static string GetContentType(string path)
         {
             string contentType;
             new FileExtensionContentTypeProvider().TryGetContentType(path, out contentType);
             return contentType ?? "application/octet-stream";
+        }
+
+        static string CalculateMD5Hash(string filePath)
+        {
+            using (var md5 = MD5.Create())
+            {
+                using (var stream = File.OpenRead(filePath))
+                {
+                    var hash = md5.ComputeHash(stream);
+                    return BitConverter.ToString(hash).Replace("-", "").ToLowerInvariant();
+                }
+            }
         }
     }
 }
