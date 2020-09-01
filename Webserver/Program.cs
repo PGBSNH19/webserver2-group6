5f3ce57891cfa9972ff9bb19d5f7baf0621f91ef
Fredrik Odén <fredrikoden87@gmail.com>
2020-09-01 15:34:10 +0200

    Added cookieCounter and dynamic counter.html page

--- Webserver/Program.cs
+++ Webserver/Program.cs
@@ -8,7 +8,6 @@ namespace Webserver
 {
     class Program
     {
-        private static int counter = 0;
         static void Main(string[] args)
         {
             SimpleListenerExample(new string[] { "http://localhost:8080/" });
@@ -22,7 +21,6 @@ namespace Webserver
                 return;
             }
             // URI prefixes are required,
-            // for example "http://contoso.com:8080/index/".
             if (prefixes == null || prefixes.Length == 0)
                 throw new ArgumentException("prefixes");
 
@@ -35,28 +33,56 @@ namespace Webserver
             }
             listener.Start();
             Console.WriteLine("Listening...");
-            while (true) {
+            while (true)
+            {
                 // Note: The GetContext method blocks while waiting for a request.
                 HttpListenerContext context = listener.GetContext();
                 HttpListenerRequest request = context.Request;
+
                 // Obtain a response object.
                 HttpListenerResponse response = context.Response;
 
                 // Add index.html if needed
                 string location = AddIndex(request.RawUrl);
 
-                // Construct a response.
-                string filePath = Path.Combine(Directory.GetCurrentDirectory() + location);
-                byte[] buffer = File.ReadAllBytes(filePath);
-                counter++;
+                // Create a new cookie if there isn't one or add +1 to the counter
+                Cookie counterCookie = request.Cookies["Counter"];
+                if (counterCookie == null)
+                    counterCookie = new Cookie("Counter", "1");
+                else
+                    counterCookie.Value = (int.Parse(counterCookie.Value) + 1).ToString();
+
+                // Correct path to make it count on the same counter on all pages
+                counterCookie.Path = "/";
                 
+                // Add the cookie to the response
+                response.Cookies.Add(counterCookie);
+                request.Cookies.Add(counterCookie);
+
+                // Construct a response.
+                byte [] buffer;
+                string filePath = Path.Combine(Directory.GetCurrentDirectory() + location);
+                if (location == "/counter.html")
+                {
+                    string responseString = $"<html><body><p style=font-size:42px;text-align:center>{counterCookie.Value}</br></br><a href=http://{request.Url.Authority}/index.html>Home</a></p></body></html>";
+                    buffer = System.Text.Encoding.UTF8.GetBytes(responseString);
+                }
+                else if (File.Exists(filePath))
+                {
+                    buffer = File.ReadAllBytes(filePath);
+                }
+                else
+                {
+                    response.StatusCode = (int)HttpStatusCode.NotFound;
+                    continue;
+                }
+
                 response.ContentLength64 = buffer.Length;
                 response.AddHeader("Expires", DateTime.Now.AddYears(1).ToString());
                 response.AddHeader("StatusCode", response.StatusCode.ToString());
                 response.AddHeader("Content-Type", GetContentType(filePath));
 
                 string contentType = GetContentType(filePath);
-                Console.WriteLine("Counter: " + counter + " " + contentType);
 
                 Stream stream = response.OutputStream;
                 stream.Write(buffer, 0, buffer.Length);
@@ -64,8 +90,8 @@ namespace Webserver
                 
                 //listener.Stop();
             }
-
         }
+
         private static string GetContentType(string path)
         {
             string contentType;
