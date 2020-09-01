d9a6fae3d08fd7cf7ba67087e1525cd17a47dec9
Fredrik Odén <fredrikoden87@gmail.com>
2020-09-01 13:19:16 +0200

    Initial

--- Webserver/Program.cs
+++ Webserver/Program.cs
@@ -8,10 +8,10 @@ namespace Webserver
     {
         static void Main(string[] args)
         {
-            SimpleListenerExample(new string[] { "http://localhost:8080/" });
+            SimpleHTTPListener(new string[] { "http://localhost:8080/" });
         }
 
-        public static void SimpleListenerExample(string[] prefixes)
+        public static void SimpleHTTPListener(string[] prefixes)
         {
             if (!HttpListener.IsSupported)
             {
@@ -25,29 +25,115 @@ namespace Webserver
 
             // Create a listener.
             HttpListener listener = new HttpListener();
+
             // Add the prefixes.
             foreach (string s in prefixes)
             {
                 listener.Prefixes.Add(s);
             }
+
+
+            // Start the listener
             listener.Start();
             Console.WriteLine("Listening...");
-            while (true) {
+
+            while (true)
+            {
                 // Note: The GetContext method blocks while waiting for a request.
                 HttpListenerContext context = listener.GetContext();
                 HttpListenerRequest request = context.Request;
-                // Obtain a response object.
-                HttpListenerResponse response = context.Response;
-                // Construct a response.
-                byte[] buffer = File.ReadAllBytes(Path.Combine(Directory.GetCurrentDirectory() + request.RawUrl));
 
-                response.ContentLength64 = buffer.Length;
-                System.IO.Stream stream = response.OutputStream;
-                stream.Write(buffer, 0, buffer.Length);
-                stream.Close();
+                // Create location string and check if we need to add Index.html
+                string location = AddIndex(request.RawUrl);
+                
+                if (!File.Exists(Path.Combine(Directory.GetCurrentDirectory() + location)))
+                    location = GetFullAdress(request);
 
-                //listener.Stop();
+                // Exit if it's a request for a file that doesn't exists
+                if (File.Exists(Path.Combine(Directory.GetCurrentDirectory() + location)))
+                {
+                    // Obtain a response object.
+                    HttpListenerResponse response = context.Response;
+
+                    // Create a new cookie if there isn't one or add +1 to the counter
+                    Cookie counterCookie = request.Cookies["Counter"];
+                    if (counterCookie == null)
+                        counterCookie = new Cookie("Counter", "1");
+                    else
+                        counterCookie.Value = (int.Parse(counterCookie.Value) + 1).ToString();
+
+                    // Correct path to make it count on the same counter on all pages
+                    counterCookie.Path = "/";
+
+                    // Add the cookie to the response
+                    response.Cookies.Add(counterCookie);
+
+
+                    // Add a response code
+                    response.StatusCode = (int)HttpStatusCode.OK;
+
+                    // Construct a response.
+                    byte[] buffer = File.ReadAllBytes(Path.Combine(Directory.GetCurrentDirectory() + location));
+                    response.ContentLength64 = buffer.Length;
+                    Stream stream = response.OutputStream;
+                    stream.Write(buffer, 0, buffer.Length);
+                    stream.Close();
+                }
+                else if (location == "/counter")
+                {
+                    // Obtain a response object.
+                    HttpListenerResponse response = context.Response;
+
+                    // Create a new cookie if there isn't one or add +1 to the counter
+                    Cookie counterCookie = request.Cookies["Counter"];
+                    if (counterCookie == null)
+                        counterCookie = new Cookie("Counter", "1");
+
+                    // Correct path to make it count on the same counter on all pages
+                    counterCookie.Path = "/";
+
+                    // Add the cookie to the response
+                    response.Cookies.Add(counterCookie);
+
+
+                    // Add a response code
+                    response.StatusCode = (int)HttpStatusCode.OK;
+
+
+                    // Construct a response.
+                    string responseString = $"<html><body><p style=font-size:42px;text-align:center>{counterCookie.Value}</br></br><a href=http://{request.Url.Authority}/content/>Home</a></p></body></html>";
+                    byte[] buffer = System.Text.Encoding.UTF8.GetBytes(responseString);
+                    response.ContentLength64 = buffer.Length;
+                    Stream stream = response.OutputStream;
+                    stream.Write(buffer, 0, buffer.Length);
+                    stream.Close();
+                }
             }
+
+            //listener.Stop();
+        }
+
+        public static string GetFullAdress(HttpListenerRequest request)
+        {
+            // Check if there's no referrer
+            if (request.UrlReferrer == null)
+                return request.RawUrl;
+
+            string location = request.UrlReferrer.ToString();
+            
+            location = location.Substring(location.IndexOf(request.Url.Authority) + request.Url.Authority.Length)
+                               .Replace("index.html", "")
+                               + request.Url.AbsolutePath;
+
+            return AddIndex(location);
+        }
+
+        public static string AddIndex(string rawUrl)
+        {
+            if (rawUrl.EndsWith('/'))
+                return rawUrl + "index.html";
+            else
+                return rawUrl;
         }
     }
 }
